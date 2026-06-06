bfdf0492afd04af010ac0c6b50e8d3753fab5f4f
Vincent <vincent.corriveau89@gmail.com>
2026-06-06 17:02:51 -0400

    changing /auth and /revoke endpoint for /login and /logout

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -6,7 +6,7 @@ interface LoginResponse {
 }
 
 export async function login(username: string, password: string): Promise<void> {
-  const res = await fetch('/api/v1/auth', {
+  const res = await fetch('/api/v1/login', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ username, password }),
